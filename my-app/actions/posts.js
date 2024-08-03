18f53dc1178488b6a00d05acaa56bca5301fef5f
Priyanka <pbhaukajee@gmail.com>
2024-08-03 16:33:31 -0500

    adjust server actions

---
+++ my-app/actions/posts.js
@@ -0,0 +1,39 @@
+"use server";
+
+import { storePost } from "@/lib/posts";
+import { redirect } from "next/navigation";
+
+//form action
+export async function createPost(prevState, formData) {
+  //gets hold of any value inputed by the user taking name attribute from the form
+  const title = formData.get("title");
+  const image = formData.get("image");
+  const content = formData.get("content");
+
+  let errors = [];
+
+  if (!title || title.trim().length === 0) {
+    errors.push("Title is required.");
+  }
+
+  if (!content || content.trim().length === 0) {
+    errors.push("Content is required.");
+  }
+
+  if (!image || image.size === 0) {
+    errors.push("Image is required.");
+  }
+
+  if (errors.length > 0) {
+    return { errors };
+  }
+
+  await storePost({
+    imageUrl: "",
+    title,
+    content,
+    userId: 1,
+  });
+
+  redirect("/feed");
+}
